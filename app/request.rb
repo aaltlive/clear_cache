46f1c2b7213f22d65e75c263fb905078dc42fc7b
aaltlive <aaltlive@gmail.com>
2020-02-17 12:09:36 +0300

    edit README

--- app/request.rb
+++ app/request.rb
@@ -13,9 +13,10 @@ def do_request()
   end
 
   for i in urls.uniq
-    # REDIS.del('all_urls') # удалить ссылки, если получены ошибки
-    # vk_clear(i) # удалить это, нужен чтобы чистить "застрявшие" ссылки
-    REDIS.rpush('all_urls', i) if not vk_clear(i)
+    begin
+      vk_clear(i)
+    rescue
+    end
   end
 
 end
@@ -24,14 +25,3 @@ while 1
   do_request()
   sleep(5)
 end
-
-# URI::InvalidURIError at /clearcache
-# URI must be ascii only "https://api.vk.com/method/pages.clearCache?url=aaasdad\u0432\u0444\u044B\u0432
-# 
-# Если запрос не удался, то вернуть ссылку в очередь. Делать так ограниченное кол-во раз
-
-# Задачи:
-# 1. Принимать несколько ссылок (в каком виде будет поступать массив ссылок?)
-# 2. Ограниченние на возврат в очередь в случае ошибки обновления кеша
-# 3. Ошибка при попытке обновить кеш у ссылки не из аскей символов
-# 4. 
