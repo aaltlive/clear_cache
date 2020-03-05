cae5acb2a24095c78b7dc37b6b4620ea70dcd156
aaltlive <aaltlive@gmail.com>
2020-03-05 11:47:52 +0300

    add 404 sinatra

--- app/server.rb
+++ app/server.rb
@@ -7,10 +7,18 @@ REDIS = Redis.new
 
 route :get, :post, '/clearcache' do
   REDIS.rpush('all_urls', params['url'])
-  json('response' => 1)
+  json("response" => 1)
 
   # dev_mode
   # REDIS.del("all_urls")
   json( REDIS.lrange('all_urls', 0, 999) )
   #
+end
+
+not_found do
+  status 404
+  json("error" => {
+    "error_code" => 404,
+    "error_msg" => "Something wrong! Try to type URL correctly"
+  })
 end
